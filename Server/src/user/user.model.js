5f95359d79b8cb4df1a9d65ea296e1e5a42bc2c4
Dipanshuk1 <dipanshukumar1936@gmail.com>
2026-01-21 10:58:42 +0530

    created user schema and model

---
+++ Server/src/user/user.model.js
@@ -0,0 +1,32 @@
+import {model, Schema} from 'mongoose';
+
+const userSchema=new Schema({
+    fullname:{
+        type:String,
+        required:true,
+        lowercase:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    password:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    status:{
+        type:Boolean,
+        default:false
+    },
+    role:{
+        type:String,
+        dafault:"user",
+        enum:["user"]
+    }
+},{timestamps:true});
+
+const UserModel = model('User', userSchema);
+export default UserModel;
