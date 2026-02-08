f2da2c167b141a7c5e8a8065e22c14b1d3a0baf2
Dipanshuk1 <dipanshukumar1936@gmail.com>
2026-02-08 13:16:00 +0530

    Updated index.js file

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,10 +1,31 @@
 import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
+dotenv.config();
+import userRouter from "./user/user.routes.js";
+import moragn from "morgan";
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hii, mr");
-});
+//database connection
+import mongoose from "mongoose";
+mongoose.connect(process.env.DB_URL)
+.then(()=>console.log("Database connected !"))
+.catch((err)=>console.log("Database connection error:",err));
+
+app.use(cookieParser());
+app.use(cors({
+  origin: process.env.DOMAIN
+}));
+
+//app level middleware
+app.use(moragn("dev"));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+//router level middleware
+app.use("/user/api", userRouter);
 
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
