e682015a65b29836cdb610afa9c4db8cf53c0558
Dipanshuk1 <dipanshukumar1936@gmail.com>
2026-02-08 13:09:00 +0530

    Added User Model

--- Server/src/user/user.model.js
+++ Server/src/user/user.model.js
@@ -1,4 +1,5 @@
 import {model, Schema} from 'mongoose';
+import bcrypt from "bcrypt";
 
 const userSchema=new Schema({
     fullname:{
@@ -15,7 +16,8 @@ const userSchema=new Schema({
     email:{
         type:String,
         required:true,
-        trim:true
+        trim:true,
+        unique:true
     },
     password:{
         type:String,
@@ -28,10 +30,15 @@ const userSchema=new Schema({
     },
     role:{
         type:String,
-        dafault:"user",
+        default:"user",
         enum:["user"]
     }
 },{timestamps:true});
+
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
+    this.password = await bcrypt.hash(this.password.toString(),12);
+});
 
 const UserModel = model('User', userSchema);
 export default UserModel;
